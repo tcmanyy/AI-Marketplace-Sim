b68bb805c94e4b48c2bbef40feddc60f74046716
SAKSHIAGRAWAL836 <sakshi.23bsa101@vitbhopal.ac.in>
2025-04-06 10:52:28 +0530

    Added React frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import UploadModel from './pages/uploadmodel';
+import Marketplace from './pages/marketplace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <a href="/">Home</a> | 
+          <a href="/login">Login</a> | 
+          <a href="/upload">Upload</a> | 
+          <a href="/marketplace">Marketplace</a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/upload" element={<UploadModel />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/marketplace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Marketplace() {
+  return <h1>Marketplace Page</h1>;
+}
+
+export default Marketplace;
